645d1ec979cd69c2f1a133236c6f08e72bff1c4c
truethatplaya <alina.hernandez.ca@gmail.com>
2021-05-03 06:06:11 -0700

    city name is appending to the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
+var apiKey = "&appid=e15cbfae7f895d1b67f2da6731580502";
 var forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-var apiKey = "&appid=e15cbfae7f895d1b67f2da6731580502";
 var city = ""; //need user input
 var requestUrlTest =
   "http://api.openweathermap.org/data/2.5/forecast?q=sacramento&appid=e15cbfae7f895d1b67f2da6731580502";
 
-// Fetching the Weather API response.
+var currentCity = document.querySelector("#currentCity");
+var currentTemp = document.querySelector("#currentTemp");
+var currentHumidity = document.querySelector("#currentHumidity");
+var currentWindSpeed = document.querySelector("#currentWindSpeed");
+var currentUV = document.querySelector("#currentUV");
+
+// Calling the Weather API to run on the click of the serach button. Fetching that response with the Test URL
 function getWeatherApi() {
   fetch(requestUrlTest)
     .then(function (response) {
@@ -13,9 +19,10 @@ function getWeatherApi() {
     })
     .then(function (response) {
       console.log(response.city.name); //this works
+      currentCity.textContent = response.city.name; //appending city to html
 
       for (var i = 0; i < response.length; i++) {
-        console.log(response.name);
+        console.log(response.length);
       }
     });
 }
@@ -28,9 +35,10 @@ function getWeatherApi() {
 //       console.log(response);
 //       console.log(response.city.name);
 
-//       // document.getElementById("quote").innerHTML = "'" + response.city.name + "'";
-//       // document.getElementById("author").innerHTML =
-//       //   "author: " + response.originator.name;
+// document.getElementById("quote").innerHTML =
+//   "'" + response.city.name + "'";
+// document.getElementById("author").innerHTML =
+//   "author: " + response.originator.name;
 //     })
 //     .catch((err) => {
 //       console.log(err);
